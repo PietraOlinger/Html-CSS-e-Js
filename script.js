1af418e1f045b14d87d9a7d9887b5acc926902b8
´´Pietra <piiiolinger@gmail.com>
2025-12-02 16:27:44 -0300

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Rolagem suave para a seção de produtos
+function scrollToProducts() {
+  document.getElementById("produtos")
+    .scrollIntoView({ behavior: "smooth" });
+}
+
+// Simulação de compra
+function comprar(produto) {
+  alert( {$produto});
+}
